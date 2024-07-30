bec9184b7c6b9a3356ff1e97c627baa72d27efbc
Oleksii But <alms27912@gmail.com>
2024-07-30 17:16:15 +0300

    fix: add border to order item image

--- src/components/OrderItem/OrderItem.styles.js
+++ src/components/OrderItem/OrderItem.styles.js
@@ -1,7 +1,11 @@
 import { styled } from '@mui/material';
+import { grey } from '@mui/material/colors';
 
 export const Image = styled('img')({
   width: '100%',
   height: '100%',
   objectFit: 'cover',
+  borderWidth: 1,
+  borderStyle: 'solid',
+  borderColor: grey[300],
 });
